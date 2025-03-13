a09cdcec84cf592fa2f7d75e43af2606b54c34ce
Артём Лазарев <artemlazarev@Mac-mini-Artem.local>
2025-03-13 15:29:57 +0500

    add pseudo dyn array

--- src/dynarray.cpp
+++ src/dynarray.cpp
@@ -27,9 +27,8 @@ class DynamicArray {
         }
 
         void pop_back() {
-            if (size > 0) {
-                size--; // ?
-            }
+            if (size > 0)
+                size--;
         }
 
         void clear() {
@@ -49,9 +48,8 @@ class DynamicArray {
 
         void removeAt(size_t index) {
             if (index < size) {
-                for (size_t i = index + 1; i < size; ++i) {
+                for (size_t i = index + 1; i < size; ++i)
                     array[i - 1] = array[i];
-                }
                 --size;
             }
         }
@@ -60,3 +58,64 @@ class DynamicArray {
             return array[i];
         }
 };
+
+// Pseudo dynamic array
+
+// template <typename T>
+// class DynamicArray {
+// private:
+//     size_t capacity;
+//     size_t size;
+//     T* array;
+// public:
+//     DynamicArray() : capacity(10), size(0), array(new T[capacity]) {}
+
+//     ~DynamicArray() {
+//         delete[] array;
+//     }
+
+//     void push_back(const T& value) {
+//         if (size == capacity) {
+//             capacity *= 2;
+//             T* t = new T[capacity];
+//             for (size_t i = 0; i < size; ++i)
+//                 t[i] = array[i];
+//             delete[] array;
+//             array = t;
+//         }
+//         array[size] = value;
+//         ++size;
+//     }
+
+//     void pop_back() {
+//         if (size > 0)
+//             --size;
+//     }
+
+//     void clear() {
+//         delete[] array;
+//         capacity = 10;
+//         size = 0;
+//         array = new T[capacity];
+//     }
+
+//     size_t getSize() const {
+//         return size;
+//     }
+
+//     bool empty() const {
+//         return size == 0;
+//     }
+
+//     void removeAt(size_t index) {
+//         if (index < size) {
+//             for (size_t i = index + 1; i < size; ++i)
+//                 array[i - 1] = array[i];
+//             --size;
+//         }
+//     }
+
+//     T& operator[](size_t i) {
+//         return array[i];
+//     }
+// };
